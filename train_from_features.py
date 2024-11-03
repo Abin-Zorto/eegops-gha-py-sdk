68e23ab6ef6fd603f0619f589e00d1401b55a736
AbinZorto <abinzorto@gmail.com>
2024-11-03 12:24:24 +0000

    chore: delete eegrai.py and train_from_features.py

--- train_from_features.py
+++
@@ -1,8 +0,0 @@
-class ModelWrapper(mlflow.pyfunc.PythonModel):
-    def __init__(self, model):
-        self.model = model
-
-    def predict(self, context, model_input):
-        # The error suggests model_input is a DataFrame but we're trying to call predict on it
-        # Instead, we should use self.model to make predictions
-        return self.model.predict(model_input) 
